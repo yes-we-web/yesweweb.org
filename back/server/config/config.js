304aa33e78bf4f183ca1ebebd38638ca96cc8682
nasri samy <nasri.samy.dev@gmail.com>
2019-12-06 15:25:58 +0100

    security .env

--- back/server/config/config.js
+++ back/server/config/config.js
@@ -1,26 +1,27 @@
-{
+require('dotenv').config();
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+    "username": process.env.USER,
+    "password": process.env.PASSWORD,
+    "database": process.env.DB,
+    "host": process.env.HOST,
+    "dialect": process.env.DIALECT,
+
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+    "username": process.env.USER,
+    "password": process.env.PASSWORD,
+    "database": process.env.DB,
+    "host": process.env.HOST,
+    "dialect": process.env.DIALECT,
+
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+    "username": process.env.USER,
+    "password": process.env.PASSWORD,
+    "database": process.env.DB,
+    "host": process.env.HOST,
+    "dialect": process.env.DIALECT,
+
   }
 }
