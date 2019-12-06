ba43006e0b9bb7a5906dff77973ee0bca57fd06e
nasri samy <nasri.samy.dev@gmail.com>
2019-12-06 16:35:54 +0100

    add router

--- back/server/routes/index.js
+++ back/server/routes/index.js
@@ -1,12 +1,14 @@
+import express from "express";
+const router = express.Router();
 const articlesController = require("../controllers").articles;
 
-module.exports = app => {
-  app.get("/api", (req, res) =>
+module.exports = router => {
+  router.get("/api", (req, res) =>
     res.status(200).send({
       message: "Welcome to the Todos API!"
     })
   );
 
-  app.post("/api/articles", articlesController.create);
-  app.get("/api/articles", articlesController.list);
+  router.post("/api/articles", articlesController.create);
+  router.get("/api/articles", articlesController.list);
 };
