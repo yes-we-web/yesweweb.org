4222043d4b440297d81aafbed9e1c0b16a51baf5
Antoine-Goncalves <goncalves.devweb@gmail.com>
2019-12-10 16:32:20 +0100

    :construction: code test for article.test.js

--- back/server/test/article.test.js
+++ back/server/test/article.test.js
@@ -1,1 +1,21 @@
-const assert = require("chai").assert;
+const chai = require("chai");
+const route = require("../routes/index");
+const chaiHttp = require("chai-http");
+const should = chai.should();
+const articles = require("../controllers").articles;
+
+chai.use(chaiHttp);
+
+describe("Articles", function() {
+  describe("/GET Article", function() {
+    it("GET status", done => {
+      chai
+        .request(articles)
+        .get("/api/articles")
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+  });
+});
