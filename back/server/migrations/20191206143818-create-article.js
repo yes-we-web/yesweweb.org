e77d358b683b6b9612e478f2564e6e6aae39354a
DRINGOT <donovan.ringot@hotmail.fr>
2019-12-11 12:07:36 +0100

    change simple quotes into double quotes in migration article

--- back/server/migrations/20191206143818-create-article.js
+++ back/server/migrations/20191206143818-create-article.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Articles', {
+    return queryInterface.createTable("Articles", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,6 +25,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Articles');
+    return queryInterface.dropTable("Articles");
   }
 };
