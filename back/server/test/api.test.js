f0ba21bae9d62496cb40f03e84537e2fa1727b2c
nasri samy <nasri.samy.dev@gmail.com>
2019-12-12 15:50:37 +0100

    test unit route get api

---
+++ back/server/test/api.test.js
@@ -0,0 +1,17 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const should = chai.should();
+const app = require("../../app");
+chai.use(chaiHttp);
+
+describe("/GET api", function() {
+  it("GET status", done => {
+    chai
+      .request(app)
+      .get("/api")
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+});
