fd216f1ba8793dd4622fedfe0f0f6b26a13c51b9
nasri samy <nasri.samy.dev@gmail.com>
2019-12-12 10:51:56 +0100

    test async await to create function

--- back/server/controllers/articles.js
+++ back/server/controllers/articles.js
@@ -2,13 +2,15 @@ const Article = require("../models").Article;
 const Comments = require("../models").Comments;
 
 module.exports = {
-  create(req, res) {
-    return Article.create({
+  async create(req, res) {
+
+    let result = await Article.create({
       title: req.body.title,
       content: req.body.content
     })
       .then((article) => res.status(201).send(article))
       .catch((error) => res.status(400).send(error));
+    return result;
   },
   // list comments in article
   list(req, res) {
