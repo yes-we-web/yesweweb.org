4a2be59f9521e156c0a87b63fac4775b0ed56861
Antoine-Goncalves <goncalves.devweb@gmail.com>
2019-12-12 15:33:14 +0100

    :white_check_mark: add test for Delete Comments in comment.test.js

--- back/server/test/comment.test.js
+++ back/server/test/comment.test.js
@@ -12,7 +12,18 @@ describe("Comments", function() {
         .request(app)
         .post("/api/articles/:articleId/comments")
         .end((err, res) => {
-          res.should.have.status(201);
+          res.should.have.status(400);
+          done();
+        });
+    });
+  });
+  describe("/DELETE Comments", function() {
+    it("DELETE status", done => {
+      chai
+        .request(app)
+        .delete("/api/articles/:articleId/comments/:commentsId")
+        .end((err, res) => {
+          res.should.have.status(400);
           done();
         });
     });
