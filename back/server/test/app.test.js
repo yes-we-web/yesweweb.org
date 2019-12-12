efeff4d2eea7681aab44fc08328eb86d914f3405
DRINGOT <donovan.ringot@hotmail.fr>
2019-12-12 15:56:26 +0100

    create test status app

--- back/server/test/app.test.js
+++ back/server/test/app.test.js
@@ -4,3 +4,20 @@ const should = chai.should();
 const app = require("../../app");
 
 chai.use(chaiHttp);
+
+describe("App", function() {
+    describe("/GET App", function() {
+      it("GET status", done => {
+        chai
+          .request(app)
+          .get("/*")
+          .end((err, res) => {
+            res.should.have.status(200);
+            done();
+          });
+      });
+    });
+});
+
+
+
