97da67882f84eba7e3aa5173cb17a42f94e8e1a6
Antoine Goncalves <goncalves.devweb@gmail.com>
2019-12-17 15:29:43 +0100

    :seedling: create controllers categories and add create function

---
+++ back/server/controllers/categories.js
@@ -0,0 +1,13 @@
+const Categories = require("../models").Categories;
+
+module.exports = {
+    async create(req, res) {
+        let result = await Categories.create({
+                content: req.body.content,
+                articleId: req.params.articleId,
+            })
+            .then((categories) => res.status(201).send(categories))
+            .catch((error) => res.status(400).send(error));
+        return result;
+    }
+}
