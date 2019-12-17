fa9724716ae05097c68d423e23075dfe7030671b
DRINGOT <donovan.ringot@hotmail.fr>
2019-12-17 15:50:25 +0100

    create update function for categories

--- back/server/controllers/categories.js
+++ back/server/controllers/categories.js
@@ -9,5 +9,28 @@ module.exports = {
             .then((categories) => res.status(201).send(categories))
             .catch((error) => res.status(400).send(error));
         return result;
-    }
+    },
+    async update(req, res) {
+        let result = await  Categories.findOne({
+            where: {
+              id: req.params.categoriesId,
+              articleId: req.params.articleId,
+            },
+          })
+          .then((categories) => {
+            if (!categories) {
+              return res.status(404).send({
+                message: "categories Not Found",
+              });
+            }
+            return categories.update({
+                content: req.body.content || categories.content,
+                complete: req.body.complete || categories.complete,
+              })
+              .then((updatedCategories) => res.status(200).send(updatedCategories))
+              .catch((error) => res.status(400).send(error));
+          })
+          .catch((error) => res.status(400).send(error));
+          return result;
+      },
 }
