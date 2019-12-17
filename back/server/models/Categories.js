e5c4e80933ab2e67f3d22fb6815bbbc54b94678a
Antoine Goncalves <goncalves.devweb@gmail.com>
2019-12-17 15:28:16 +0100

    :seedling: create Categories table and migrate. Add contents in models/Categories and migrate file

---
+++ back/server/models/Categories.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Categories = sequelize.define('Categories', {
+    content: DataTypes.STRING
+  }, {});
+  Categories.associate = function (models) {
+    Categories.belongsTo(models.Article, {
+      foreignKey: 'articleId',
+      onDelete: 'CASCADE',
+    });
+  };
+  return Categories;
+};
