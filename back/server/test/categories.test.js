0ec7ad455ed3dd464a9c9f41bc6f9954bd0afc2d
nasri samy <nasri.samy.dev@gmail.com>
2019-12-18 12:23:28 +0100

    test categories

--- back/server/test/categories.test.js
+++ back/server/test/categories.test.js
@@ -4,3 +4,17 @@ const should = chai.should();
 const app = require("../../app");
 
 chai.use(chaiHttp);
+
+describe("Categories", function() {
+  describe("/POST Categories", function() {
+    it("POST status", done => {
+      chai
+        .request(app)
+        .post("/api/articles/:articleId/categories")
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+  });
+});
