d52ca556c9127ff1e9a098ee4e10036ec505c1db
nasri samy <nasri.samy.dev@gmail.com>
2020-01-06 17:04:25 +0100

    :fire: fix syntax

--- back/server/controllers/users.js
+++ back/server/controllers/users.js
@@ -174,9 +174,9 @@ module.exports = {
         return user
           .destroy()
           .then(() => res.status(204).send())
-          .catch(error => res.status(400).send(error));
+          .catch((error) => res.status(400).send(error));
       })
-      .catch(error => res.status(400).send(error));
+      .catch((error) => res.status(400).send(error));
     return result;
   }
 };
