c6438af43f73743140e89b74c4b247013ac05ae7
Antoine-Goncalves <goncalves.devweb@gmail.com>
2020-01-06 11:11:12 +0100

    :pencil: add const for users controllers and create function register and login empty

---
+++ back/server/controllers/users.js
@@ -0,0 +1,9 @@
+const bcrypt = require("bcrypt");
+const jwtUtils = require("../utils/jwt.utils");
+const asyncLib = require("async");
+const Users = require("../models/").Users;
+
+module.exports = {
+  register: function(req, res) {},
+  login: function(req, res) {}
+};
