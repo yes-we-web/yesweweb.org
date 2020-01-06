a7393f4b9fd6755ce38fbfb833858ed4f9f78214
nasri samy <nasri.samy.dev@gmail.com>
2020-01-06 16:37:30 +0100

    :fire: add delete users

--- back/server/routes/index.js
+++ back/server/routes/index.js
@@ -16,8 +16,10 @@ module.exports = router => {
 
   router.post("/api/users/register", usersController.register);
   router.post("/api/users/login", usersController.login);
-  router.get("/api/users/profile", usersController.getUserProfile)
-  router.put("/api/users/profile", usersController.updateUserProfile)
+  router.get("/api/users/profile", usersController.getUserProfile);
+  router.put("/api/users/profile", usersController.updateUserProfile);
+  router.get("/api/users", usersController.list);
+  router.delete("/api/users/:userId", usersController.destroy);
   //Articles
   router.post("/api/articles", articlesController.create);
   router.get("/api/articles", articlesController.list);
