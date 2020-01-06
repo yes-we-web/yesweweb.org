76eedce0ff5ab9836ff7be90836d99d87111bbd2
nasri samy <nasri.samy.dev@gmail.com>
2020-01-06 12:20:41 +0100

    :fire: sign Token

--- back/utils/jwt.utils.js
+++ back/utils/jwt.utils.js
@@ -1,5 +1,10 @@
 const jwt = require("jsonwebtoken");
-
+const JWT_SIGN_SECRET = "08jisjd0ydauiahddhjnsdqjdnao5jaijd7382jadaijowolf69";
 module.exports = {
-  generateTokenForUser: function(userData) {}
+  generateTokenForUser: function(userData) {
+    return jwt.sign({
+      userId: userData.id,
+      isAdmin: userData.isAdmin
+    });
+  }
 };
