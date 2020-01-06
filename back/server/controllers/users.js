da06d1038a629d9c089025df0c1a81a7a518ef9d
Antoine-Goncalves <goncalves.devweb@gmail.com>
2020-01-06 11:20:47 +0100

    :fire: add values for register in controllers/users

--- back/server/controllers/users.js
+++ back/server/controllers/users.js
@@ -4,6 +4,20 @@ const asyncLib = require("async");
 const Users = require("../models/").Users;
 
 module.exports = {
-  register: function(req, res) {},
+  register: function(req, res) {
+    const lastname = req.body.lastname;
+    const firstname = req.body.firstname;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if (
+      email == null ||
+      firstname == null ||
+      lastname == null ||
+      password == null
+    ) {
+      return res.status(400).json({ error: "missing parameters" });
+    }
+  },
   login: function(req, res) {}
 };
