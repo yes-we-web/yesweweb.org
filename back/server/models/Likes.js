c00e930546169e3bd14623baf8f10d4513b4dcac
DRINGOT <donovan.ringot@hotmail.fr>
2020-01-08 09:21:49 +0100

    create likes association between users & articles

--- back/server/models/Likes.js
+++ back/server/models/Likes.js
@@ -1,11 +1,48 @@
-'use strict';
+"use strict";
+
 module.exports = (sequelize, DataTypes) => {
-  const Likes = sequelize.define('Likes', {
-    articleId: DataTypes.INTEGER,
-    userId: DataTypes.INTEGER
-  }, {});
+  const Likes = sequelize.define(
+    "Likes",
+    {
+      articleId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "Articles",
+          key: "id"
+        }
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "Users",
+          key: "id"
+        }
+      }
+    },
+    {}
+  );
   Likes.associate = function(models) {
-    // associations can be defined here
+    Jsers.belongsToMany(models.Articles, {
+      through: models.Likes,
+      foreignKey: "userId",
+      otherKey: "articleId"
+    });
+
+    Articles.belongsToMany(models.Users, {
+      through: models.Likes,
+      foreignKey: "articleId",
+      otherKey: "userId"
+    });
+
+    Likes.belongsTo(models.Users, {
+      foreignKey: "userId",
+      as: "users"
+    });
+
+    Likes.belongsTo(models.Articles, {
+      foreignKey: "articleId",
+      as: "articles"
+    });
   };
   return Likes;
 };
