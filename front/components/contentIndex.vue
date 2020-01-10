920a70de0eb01cef934ad960078416bb62f1bed5
nasri samy <nasri.samy.dev@gmail.com>
2020-01-10 18:23:46 +0100

    change text

--- front/components/contentIndex.vue
+++ front/components/contentIndex.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
-    <v-container>
-      <h1>Description de la formation</h1>
-      <p>L’Institut Vitamine T propose une formation d’une durée de 8 mois, dont 1 mois d’immersion professionnelle, au métier de développeur web dont le contenu pédagogique est conçu par SIMPLON. Ce parcours vise l’obtention du Titre RNCP de niveau III de Développeur web et web mobile. En s’appuyant sur les principes de la méthode agile, cette formation permet d’acquérir des compétences en développement front-end et back-end, des notions liées à la sécurité informatique et au RGPD et de maîtriser des technologies recherchées par les recruteurs (Node.js, Sinatra, Heroku, MongoDB, …).</p>
-    </v-container>
+    <v-row align="center" justify="center">
+      <v-col class="text-center" cols="12">
+        <v-container>
+          <h1 style="color:#55b794">Description de la formation</h1>
+          <p>L’Institut Vitamine T propose une formation d’une durée de 8 mois, dont 1 mois d’immersion professionnelle, au métier de développeur web dont le contenu pédagogique est conçu par SIMPLON. Ce parcours vise l’obtention du Titre RNCP de niveau III de Développeur web et web mobile. En s’appuyant sur les principes de la méthode agile, cette formation permet d’acquérir des compétences en développement front-end et back-end, des notions liées à la sécurité informatique et au RGPD et de maîtriser des technologies recherchées par les recruteurs (Node.js, Sinatra, Heroku, MongoDB, …).</p>
+          <span class="display-3" style="color:#55b794">-</span>
+          <br />
+        </v-container>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
