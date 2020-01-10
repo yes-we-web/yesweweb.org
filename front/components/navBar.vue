c49457c0f4b8630a924a50f2003532fa91f76167
nasri samy <nasri.samy.dev@gmail.com>
2020-01-10 16:10:46 +0100

    :fire: create navbar

--- front/components/navBar.vue
+++ front/components/navBar.vue
@@ -3,31 +3,63 @@
     :clipped-left="clipped"
     fixed
     app
-    color="#6A76AB"
+    color="#55b794"
     dark
     shrink-on-scroll
     prominent
-    src="https://picsum.photos/1920/1080?random"
+    src="YWW.png"
     fade-img-on-scroll
   >
     <template v-slot:img="{ props }">
-      <v-img v-bind="props" gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"></v-img>
+      <v-img v-bind="props"></v-img>
     </template>
 
-    <v-toolbar-title>Yes We Web</v-toolbar-title>
+    <v-toolbar-title>
+      <span>Yes</span>
+      <span class="font-weight-light">we</span>
+      <span>web</span>
+    </v-toolbar-title>
 
     <v-spacer></v-spacer>
-
+    <v-text-field sm flat hide-details label="Search" style="max-width: 12vw " color="white">
+      <input
+        id="search"
+        type="text"
+        class="ds-input"
+        autocomplete="off"
+        spellcheck="false"
+        role="combobox"
+        aria-autocomplete="list"
+        aria-expanded="false"
+        aria-owns="algolia-autocomplete-listbox-0"
+        style
+        dir="auto"
+      />
+    </v-text-field>
     <v-btn icon>
-      <v-icon>mdi-magnify</v-icon>
+      <v-icon large>mdi-magnify</v-icon>
     </v-btn>
 
     <template v-slot:extension>
-      <v-tabs align-with-title background-color="transparent">
-        <v-tab>Catégories</v-tab>
-        <v-tab>Articles</v-tab>
-        <v-tab>Nos Partenaires</v-tab>
+      <v-tabs align-with-title background-color="transparent" color="white">
+        <v-tab>
+          <li>
+            <nuxt-link tag="li" to="/">Accueil</nuxt-link>
+          </li>
+        </v-tab>
+
+        <v-tab>
+          <li>
+            <nuxt-link tag="li" to="/articles">Articles</nuxt-link>
+          </li>
+        </v-tab>
+
+        <v-tab>Partenaires</v-tab>
       </v-tabs>
+
+      <v-btn icon>
+        <v-icon large>mdi-account</v-icon>
+      </v-btn>
     </template>
   </v-app-bar>
 </template>
@@ -37,4 +69,8 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+li {
+  text-decoration: none;
+  list-style-type: none;
+}
 </style>
