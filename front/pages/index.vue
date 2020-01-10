b86127607718e35f16f1555b1d114e254967b0fa
nasri samy <nasri.samy.dev@gmail.com>
2020-01-10 17:33:49 +0100

    add composent map

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,20 +1,23 @@
 <template>
   <div>
     <parralax />
-
-    <v-parallax height="500" width="100vw" src="bannerYWW.jpg"></v-parallax>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.
+    <contentIndex />
+    <map />
   </div>
 </template>
 
 <script>
 import parralax from "@/components/parralax.vue";
 import parralax2 from "@/components/parralax2.vue";
+import contentIndex from "@/components/contentIndex.vue";
+import map from "@/components/map.vue";
 
 export default {
   components: {
     parralax,
-    parralax2
+    parralax2,
+    contentIndex,
+    map
   }
 };
 </script>
