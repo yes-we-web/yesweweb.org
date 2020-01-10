9e69eb6ddfd2133bee0aacddff9542a304177ace
nasri samy <nasri.samy.dev@gmail.com>
2020-01-10 16:11:22 +0100

    :fire: add component

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,7 +1,25 @@
-<template></template>
+<template>
+  <div>
+    <parralax />Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.
+    <v-parallax
+      height="500"
+      width="100vw"
+      src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"
+    ></v-parallax>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cupiditate, unde itaque iste perferendis ipsum autem praesentium, eveniet sit ducimus eius. Suscipit odio itaque perferendis molestiae nulla quis, optio ad.
+  </div>
+</template>
 
 <script>
+import parralax from "@/components/parralax.vue";
+import parralax2 from "@/components/parralax2.vue";
+
 export default {
-  components: {}
+  components: {
+    parralax,
+    parralax2
+  }
 };
 </script>
