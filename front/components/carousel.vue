a67c20fe5763df14e25987a71e985f8ebc75fdab
DRINGOT <donovan.ringot@hotmail.fr>
2020-01-11 16:11:59 +0100

    add new img to carousel

--- front/components/carousel.vue
+++ front/components/carousel.vue
@@ -68,6 +68,9 @@ export default {
         },
         {
           src: "CarouselYesWeWeb/yesWeWeb2.jpg "
+        },
+        {
+          src: "CarouselYesWeWeb/ forumFondationDesPossibles.jpg "
         }
       ]
     };
