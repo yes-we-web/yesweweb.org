37149a3415dac3a1e4f872ee9a572ede72e8ec42
nasri samy <nasri.samy.dev@gmail.com>
2020-01-13 15:43:06 +0100

    :fire: change parralax to parallax

---
+++ front/components/parallax2.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-parallax dark src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+    <v-row align="center" justify="center">
+      <v-col class="text-center" cols="12">
+        <h1 class="display-3 font-weight-thin mb-4">ARTICLES</h1>
+        <h4 class="subheading"></h4>
+      </v-col>
+    </v-row>
+  </v-parallax>
+</template>
