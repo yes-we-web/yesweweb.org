dcbfa5deaa691b113aeab7b7a7a613d954e6ed27
Antoine-Goncalves <goncalves.devweb@gmail.com>
2020-01-13 15:02:51 +0100

    :fire: delete search bar

--- front/components/navBar.vue
+++ front/components/navBar.vue
@@ -3,50 +3,18 @@
     :clipped-left="clipped"
     fixed
     app
-    color="#55b794"
     dark
     shrink-on-scroll
     prominent
     src="YWW.png"
     fade-img-on-scroll
+    height="2rem"
   >
     <template v-slot:img="{ props }">
       <v-img v-bind="props"></v-img>
     </template>
 
-    <v-toolbar-title>
-      <span>Yes</span>
-      <span class="font-weight-light">We</span>
-      <span>Web</span>
-    </v-toolbar-title>
-
     <v-spacer></v-spacer>
-    <v-text-field
-      sm
-      flat
-      hide-details
-      label="Search"
-      style="max-width: 12vw "
-      color="white"
-      class="mt-4"
-    >
-      <input
-        id="search"
-        type="text"
-        class="ds-input"
-        autocomplete="off"
-        spellcheck="false"
-        role="combobox"
-        aria-autocomplete="list"
-        aria-expanded="false"
-        aria-owns="algolia-autocomplete-listbox-0"
-        style
-        dir="auto"
-      />
-    </v-text-field>
-    <v-btn icon class="mt-3">
-      <v-icon>mdi-magnify</v-icon>
-    </v-btn>
 
     <template v-slot:extension>
       <v-tabs align-with-title background-color="transparent" color="white">
