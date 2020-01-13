92672b74eb98147421bd4723bd02f8c4bf80d70c
DRINGOT <donovan.ringot@hotmail.fr>
2020-01-13 14:09:42 +0100

    change link for icons footer

--- front/components/footerYWW.vue
+++ front/components/footerYWW.vue
@@ -10,8 +10,25 @@
       style="width:100%"
     >
       <v-card-text>
-        <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" icon>
-          <v-icon size="24px">{{ icon }}</v-icon>
+        <v-btn class="mx-4 white--text" icon>
+          <a class="link mx-4" href="https://fr-fr.facebook.com/YesWeWeb/"
+            ><v-icon size="24px" class="fab fa-facebook"></v-icon
+          ></a>
+          <a class="link mx-4" href="https://twitter.com/YesWeWebVT"
+            ><v-icon size="24px" class="fab fa-twitter"></v-icon
+          ></a>
+          <a
+            class="link mx-4"
+            href="mailto:mathilde.debeaune@groupevitaminet.com"
+          >
+            <v-icon size="24px" class="fab fa-google-plus"></v-icon
+          ></a>
+          <a class="link mx-4" href="https://fr.linkedin.com/company/yes-we-web"
+            ><v-icon size="24px" class="fab fa-linkedin"></v-icon
+          ></a>
+          <a class="link mx-4" href="https://github.com/yes-we-web"
+            ><v-icon size="24px" class="fab fa-github"></v-icon
+          ></a>
         </v-btn>
       </v-card-text>
 
@@ -32,21 +49,15 @@
 </template>
 
 <script>
-export default {
-  data: () => ({
-    icons: [
-      "fab fa-facebook",
-      "fab fa-twitter",
-      "fab fa-google-plus",
-      "fab fa-linkedin",
-      "fab fa-github"
-    ]
-  })
-};
+export default {};
 </script>
 
 <style lang="scss" scoped>
 p {
   text-shadow: 1px 2px 2px black;
+}
+.link {
+  text-decoration: none;
+  color: white;
 }
 </style>
