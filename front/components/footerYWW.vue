796b26a186b2c251b989eef13c6c4be07531a2fa
Antoine-Goncalves <goncalves.devweb@gmail.com>
2020-01-14 09:19:37 +0100

    :fire: change text of footerYWW

--- front/components/footerYWW.vue
+++ front/components/footerYWW.vue
@@ -1,57 +1,37 @@
 <template>
   <v-footer app padless absolute>
-    <link
-      href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
-      rel="stylesheet"
-    />
-    <v-card
-      class=" white--text text-center"
-      color="transparent"
-      style="width:100%"
-    >
+    <link href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet" />
+    <v-card class="white--text text-center" color="transparent" style="width:100%">
       <v-card-text>
-        <v-btn class=" mx-4 white--text" icon>
-          <a
-            class="link mx-4"
-            href="https://fr-fr.facebook.com/YesWeWeb/"
-            target="_blank"
-          >
+        <v-btn class="mx-4 white--text" icon>
+          <a class="link mx-4" href="https://fr-fr.facebook.com/YesWeWeb/" target="_blank">
             <v-icon size="24px" class="fab fa-facebook"></v-icon>
           </a>
         </v-btn>
-        <v-btn class=" mx-4 white--text" icon>
-          <a
-            class="link mx-4"
-            href="https://twitter.com/YesWeWebVT"
-            target="_blank"
-          >
-            <v-icon size="24px" class="fab fa-twitter"></v-icon> </a></v-btn
-        ><v-btn class=" mx-4 white--text" icon>
-          <a
-            class="link mx-4"
-            href="mailto:mathilde.debeaune@groupevitaminet.com"
-          >
-            <v-icon size="24px" class="fab fa-google-plus"></v-icon> </a></v-btn
-        ><v-btn class=" mx-4 white--text" icon>
-          <a
-            class="link mx-4"
-            href="https://fr.linkedin.com/company/yes-we-web"
-            target="_blank"
-          >
-            <v-icon size="24px" class="fab fa-linkedin"></v-icon> </a></v-btn
-        ><v-btn class=" mx-4 white--text" icon>
-          <a
-            class="link mx-4"
-            href="https://github.com/yes-we-web"
-            target="_blank"
-          >
+        <v-btn class="mx-4 white--text" icon>
+          <a class="link mx-4" href="https://twitter.com/YesWeWebVT" target="_blank">
+            <v-icon size="24px" class="fab fa-twitter"></v-icon>
+          </a>
+        </v-btn>
+        <v-btn class="mx-4 white--text" icon>
+          <a class="link mx-4" href="mailto:mathilde.debeaune@groupevitaminet.com">
+            <v-icon size="24px" class="fab fa-google-plus"></v-icon>
+          </a>
+        </v-btn>
+        <v-btn class="mx-4 white--text" icon>
+          <a class="link mx-4" href="https://fr.linkedin.com/company/yes-we-web" target="_blank">
+            <v-icon size="24px" class="fab fa-linkedin"></v-icon>
+          </a>
+        </v-btn>
+        <v-btn class="mx-4 white--text" icon>
+          <a class="link mx-4" href="https://github.com/yes-we-web" target="_blank">
             <v-icon size="24px" class="fab fa-github"></v-icon>
           </a>
         </v-btn>
       </v-card-text>
 
       <v-card-text class="white--text pt-0">
-        <p>Fait avec 💖 by the Wolf Team (Donovan, Antoine, Samy) 🇫🇷</p>
+        <p>Fait avec 💖 par Donovan, Antoine et Samy 🇫🇷</p>
       </v-card-text>
 
       <v-divider></v-divider>
