162ec2c90eb7e0479583f3e818e0215a8ee78c5d
nasri samy <nasri.samy.dev@gmail.com>
2020-01-14 13:39:00 +0100

    change style in content

--- front/components/contentIndex.vue
+++ front/components/contentIndex.vue
@@ -8,7 +8,7 @@
               class="fiche"
               :width="1300"
               :height="250"
-              :elevation="24"
+              :elevation="0"
               :color="color"
               :tile="tile"
             >
@@ -16,7 +16,19 @@
                 <v-col class="text-center" cols="12">
                   <v-container>
                     <h1 style="color:#55b794">Description de la formation</h1>
-                    <p>L’Institut Vitamine T propose une formation d’une durée de 8 mois, dont 1 mois d’immersion professionnelle, au métier de développeur web dont le contenu pédagogique est conçu par SIMPLON. Ce parcours vise l’obtention du Titre RNCP de niveau III de Développeur web et web mobile. En s’appuyant sur les principes de la méthode agile, cette formation permet d’acquérir des compétences en développement front-end et back-end, des notions liées à la sécurité informatique et au RGPD et de maîtriser des technologies recherchées par les recruteurs (Html, Css, Javascript, Vue.js, Node.js, …).</p>
+                    <p>
+                      L’Institut Vitamine T propose une formation d’une durée de
+                      8 mois, dont 1 mois d’immersion professionnelle, au métier
+                      de développeur web dont le contenu pédagogique est conçu
+                      par SIMPLON. Ce parcours vise l’obtention du Titre RNCP de
+                      niveau III de Développeur web et web mobile. En s’appuyant
+                      sur les principes de la méthode agile, cette formation
+                      permet d’acquérir des compétences en développement
+                      front-end et back-end, des notions liées à la sécurité
+                      informatique et au RGPD et de maîtriser des technologies
+                      recherchées par les recruteurs (Html, Css, Javascript,
+                      Vue.js, Node.js, …).
+                    </p>
                     <span class="display-3" style="color:#55b794">-</span>
                     <br />
                   </v-container>
@@ -36,7 +48,7 @@ export default {};
 
 <style scoped>
 .fiche {
-  border-radius: 1.5rem;
+  border-top: 3px #55b794 solid;
 }
 
 h1 {
