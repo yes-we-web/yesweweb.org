16b23ee0312d8424f7862d7baf7ac86b2395e2a8
Antoine-Goncalves <goncalves.devweb@gmail.com>
2020-01-15 08:35:18 +0100

    :bug: :fire: delete justify in renseignement.vue bug

--- front/components/renseignement.vue
+++ front/components/renseignement.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-row class="ml-12">
       <v-col cols="12">
-        <v-row :align="alignment" :justify="justify" style="height: auto;">
+        <v-row :align="alignment" style="height: auto;">
           <div class="map mt-6">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2532.975134435083!2d3.116467015571833!3d50.59041547949489!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c2d6f35265a399%3A0x90bd89b34429efcd!2s2%20Boulevard%20Thomson%2C%2059810%20Lesquin!5e0!3m2!1sfr!2sfr!4v1578741845000!5m2!1sfr!2sfr"
@@ -34,24 +34,23 @@
                       <span class="display-2">-</span>
                       <p class="font-weight-bold">
                         2 Boulevard Thomson
-                        <br />59810 <br />Lesquin <br />Hauts-de-France
+                        <br />59810
+                        <br />Lesquin
+                        <br />Hauts-de-France
                       </p>
                       <h2>Contact</h2>
                       <span class="display-2">-</span>
                       <p class="font-weight-bold">
                         Tel :
                         <br />
-                        <a class="link" href="tel:+33320617070"
-                          >03 20 61 70 70</a
-                        >
+                        <a class="link" href="tel:+33320617070">03 20 61 70 70</a>
 
                         <br />Email :
                         <br />
                         <a
                           class="link"
                           href="mailto:mathilde.debeaune@groupevitaminet.com"
-                          >mathilde.debeaune@groupevitaminet.com</a
-                        >
+                        >mathilde.debeaune@groupevitaminet.com</a>
 
                         <br />Site web :
                         <br />
@@ -59,8 +58,7 @@
                           class="link"
                           href="https://simplon.co/groupe-vitamine-t/"
                           target="_blank"
-                          >https://simplon.co/groupe-vitamine-t/</a
-                        >
+                        >https://simplon.co/groupe-vitamine-t/</a>
                       </p>
                     </div>
                   </v-sheet>
