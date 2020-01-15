c6184530099f1d9bc184aafe537ff03b0c5503d0
Antoine-Goncalves <goncalves.devweb@gmail.com>
2020-01-15 08:28:46 +0100

    :fire: delete tile in v-sheet tag in contentIndex.vue

--- front/components/contentIndex.vue
+++ front/components/contentIndex.vue
@@ -20,14 +20,7 @@
             <v-col class="text-center" cols="12">
               <v-container>
                 <v-row justify="space-around">
-                  <v-sheet
-                    class="fiche"
-                    :width="1300"
-                    :height="250"
-                    :elevation="0"
-                    :color="color"
-                    :tile="tile"
-                  >
+                  <v-sheet class="fiche" :width="1300" :height="250" :elevation="0" :color="color">
                     <v-row align="center" justify="center">
                       <v-col class="text-center" cols="12">
                         <v-container>
