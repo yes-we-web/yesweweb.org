5494826227ca7caad041719da9376aa96255732f
nasri samy <nasri.samy.dev@gmail.com>
2020-01-15 10:04:18 +0100

    :fire: add parallax to partenaires pages

---
+++ front/components/parallaxPartenaires.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-parallax dark src="partenaires.jpg" style="height:85vh">
+    <v-row align="center" justify="center">
+      <v-col class="text-center" cols="12">
+        <h4 class="subheading font-weight-bold title" style="color:#55b794"></h4>
+        <br />
+        <h1 class="display-2 font-weight-thin mb-4 font-weight-bold">
+          Découvrez nos partenaires
+          <br />
+          <span style="color:#55b794">-</span>
+        </h1>
+        <h4 class="title font-weight-bold" style="text-shadow:1px 1px 1px black">
+          Yes We Web s'engage auprès des acteurs de l'écosystème
+          <br />pour faire du numérique tout en favorisant l'insertion professionnelle
+        </h4>
+      </v-col>
+    </v-row>
+  </v-parallax>
+</template>
+
+<style scoped>
+h1 {
+  text-shadow: 2px 3px 3px black;
+}
+</style>
--- front/pages/partenaires.vue
+++ front/pages/partenaires.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
+    <parallaxPartenaires />
     <partenaires />
   </div>
 </template>
 
 <script>
 import partenaires from "@/components/partenaires.vue";
+import parallaxPartenaires from "@/components/parallaxPartenaires.vue";
 
 export default {
   components: {
-    partenaires
+    partenaires,
+    parallaxPartenaires
   }
 };
 </script>
