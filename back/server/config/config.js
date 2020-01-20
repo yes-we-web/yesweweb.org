882df777fcef83d4c406510cefe7861ed6a3eca1
nasri samy <nasri.samy.dev@gmail.com>
2020-01-20 22:21:12 +0100

    refact config js

--- back/server/config/config.js
+++ back/server/config/config.js
@@ -1,27 +1,24 @@
 require("dotenv").config();
 module.exports = {
-  "development": {
-    "username": process.env.USER,
-    "password": process.env.PASSWORD,
-    "database": process.env.DB,
-    "host": process.env.HOST,
-    "dialect": process.env.DIALECT,
-
+  development: {
+    username: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DB,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT
   },
-  "test": {
-    "username": process.env.USER,
-    "password": process.env.PASSWORD,
-    "database": process.env.DB,
-    "host": process.env.HOST,
-    "dialect": process.env.DIALECT,
-
+  test: {
+    username: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DB,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT
   },
-  "production": {
-    "username": process.env.USER,
-    "password": process.env.PASSWORD,
-    "database": process.env.DB,
-    "host": process.env.HOST,
-    "dialect": process.env.DIALECT,
-
+  production: {
+    username: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DB,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT
   }
 };
